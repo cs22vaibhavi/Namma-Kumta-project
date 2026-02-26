8032395fcf31ec68fbd5905550ec7b72211254a4
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-02-26 22:03:44 +0530

    Add files via upload

---
+++ Kumta.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Kumta.css";
+
+const Kumta=()=>{
+    return(
+        <div className="container">
+            <div class="r1">
+                <img src="https://png.pngtree.com/png-vector/20240822/ourmid/pngtree-beach-logo-design-inspiration-sunset-coconut-trees-palms-on-the-vector-png-image_13564239.png"width="35px" height="35px"></img>
+                <a>Namma Kumata</a>
+            <div class="im1">
+                <img src="https://img.icons8.com/?size=20&id=eMfeVHKyTnkc&format=png"></img>
+            </div>
+            <div class="im2">
+                <img src="https://img.icons8.com/?size=20&id=zxB19VPoVLjK&format=png"></img>
+            </div>
+            <div class="im3">
+                <img src="https://img.icons8.com/?size=20&id=K6FkUVH0GtOD&format=png"></img>
+            </div>
+            </div>
+            <div class="r2">
+                <img src="https://img.icons8.com/?size=25&id=15815&format=png"></img>
+                <b>State Bank of India</b>
+            </div>
+            <div class="r3">
+                <img src="bank.jpg"></img>
+                <a>State Bank of India - Kumta Branch</a>
+                <b>State Bank of India - Kumta Branch</b>
+                <h4>NH 66 Kumta,Uttara Kannada,Karnataka,581343</h4>
+            <div class="pic">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoZ8JyZ8k6ww6PFfJVXvyoIAymhzNOgAdJ4pnD-FEEfRQgYJPtY3vEb8gvCL3hY0RQA4&usqp=CAU"width="15px" height="15px"></img>
+                <b>+ 91 8386-224678</b>
+                <button>4.5 ★</button>
+            </div>
+            </div>
+            <div class="r4">
+                <a>More Details</a>
+            </div>
+            <div class="r5">
+                <img src="bank.jpg"></img>
+                <a>State Bank of India - Kumta Main Road Branch</a>
+                <b>State Bank of India - Kumta Main Road Branch</b>
+                <h4>Main Road,Near Market kumta, Karnataka,581343</h4>
+            </div>
+            
+            <div class="pic2">
+                
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHoZ8JyZ8k6ww6PFfJVXvyoIAymhzNOgAdJ4pnD-FEEfRQgYJPtY3vEb8gvCL3hY0RQA4&usqp=CAU"width="15px" height="15px"></img>
+                <b>+ 91 8386-224678</b>
+                <button>4.5 ★</button>
+            </div> 
+            <div class="r6">
+                <a>More Details</a>
+                
+            </div>
+        </div>
+
+    )
+}
+export default Kumta;
---
+++ Kumta.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Kumta from "./Kumta"
+
+export default {
+    title:"Component/Kumta",
+    component:Kumta,
+};
+const Template=(args)=><Kumta {...args}/>
+export const notes=Template.bind()
